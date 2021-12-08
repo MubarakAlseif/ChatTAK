020706d267b9e19987adcd2c3551adf6eaa9adab
Mobarak.Alseif@tuwaiq.edu.sa <Mobarak.Alseif@tuwaiq.edu.sa>
2021-12-08 12:13:52 +0300

    importing FirebaseFirestore.

---
+++ ChatTAK/MODEL/UserApi.swift
@@ -0,0 +1,35 @@
+//
+//  UserApi.swift
+//  ChatTAK
+//
+//  Created by Mobark Alseif on 04/05/1443 AH.
+//
+
+import Foundation
+import FirebaseFirestore
+
+class UserApi {
+    
+    static func addUser(name:String,uid:String,phone:String,email:String,completion: @escaping (Bool) -> Void) {
+        
+        let refUsers = Firestore.firestore().collection("Users")
+        
+        
+        refUsers.document(uid).setData(User.CreateUser(phone: phone, name: name, email: email))
+        
+        completion(true)
+        
+    }
+    static func getUser(uid:String,completion: @escaping (User) -> Void) {
+       
+        let refUsers = Firestore.firestore().collection("Users")
+        
+        refUsers.document(uid).getDocument { document, error in
+            if let document = document, document.exists {
+                let user = User.getUser(dict: document.data()!)
+                completion(user)
+            }
+        }
+        
+    }
+}
--- ChatTAK/ViewController.swift
+++ ChatTAK/ViewController.swift
@@ -7,23 +7,26 @@
 
 import UIKit
 import FirebaseAuth
+import FirebaseFirestore
 
 class ViewController: UIViewController {
-
+    
     
     @IBOutlet weak var email: UITextField!
     @IBOutlet weak var password: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        UserApi.getUser(uid: Auth.auth().currentUser?.uid ?? "") { user in
+            print(user.name)
+        }
         // Do any additional setup after loading the view.
     }
-
-
+    
     func SignIn(email: String,password:String) {
-      
+        
         Auth.auth().signIn(withEmail: email, password: password) { [weak self] authResult, error in
-
+            
             if let error = error {
                 print(error.localizedDescription)
             }
@@ -32,22 +35,26 @@ class ViewController: UIViewController {
             }
             print("email:\(String(describing: authResult?.user.email))")
             print("uid:\(String(describing: authResult?.user.uid))")
-            self?.performSegue(withIdentifier: "Home", sender: nil)
-          // ...
+            // ...
         }
-        
     }
+    
     func SignUp(email: String,password:String) {
         Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
             
             print("email:\(String(describing: authResult?.user.email))")
             print("uid:\(String(describing: authResult?.user.uid))")
             
-            
-          // ...
+            UserApi.addUser(name: "Mubarak", uid: authResult?.user.uid ?? "", phone: "966560504008", email: email) { check in
+                if check {
+                    print("Done saving in Database")
+                } else {
+                    
+                }
+            }
         }
-        
     }
+    
     @IBAction func signButton(_ sender: UIButton) {
         
         SignUp(email: email.text ?? "", password: password.text ?? "")
@@ -55,14 +62,9 @@ class ViewController: UIViewController {
     }
     @IBAction func singIn(_ sender: UIButton) {
         
-        
         SignIn(email: email.text ?? "", password: password.text ?? "")
         
-        
-        
-        
     }
-    
     
 }
 
